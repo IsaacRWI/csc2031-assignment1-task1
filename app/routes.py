166c0f4b03918a04f54cf471f47cc458a8a0a6a6
IsaacRWI <isaacngrwi@gmail.com>
2025-11-08 19:01:19 +0000

    Added method to sanitise user entered content

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.forms import RegistrationForm
+from bleach import clean
+
+safe_tags = {"b", "i", "u", "em", "strong", "a", "p", "ul", "ol", "li"}
 
 main = Blueprint('main', __name__)
 
@@ -10,8 +13,16 @@ def home():
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm
-    client_ip = request.remote_addr or "Unknown IP"
+    sanitized_content = None
+    if form.validate_on_submit() and form.bio.data:
+        bio_content = form.bio.data
+        sanitized_content = clean(bio_content, tags=safe_tags, attributes={'a': ['href', 'title']}, strip=True)
+        flash("Registratiom Success")
+    elif request.method == "POST":
+        pass
+
+    # client_ip = request.remote_addr or "Unknown IP"
 
 
     # name = request.form.get('name') if request.method == 'POST' else ''
-    # return render_template('register.html', name=name)
+    return render_template('register.html', form=form, bio=sanitized_content)
