b858d952a2e07b6327c1e9d09fd084d0cb67c879
IsaacRWI <isaacngrwi@gmail.com>
2025-11-09 15:11:06 +0000

    Fixed os get env returning None instead of key

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
-import dotenv
+from dotenv import load_dotenv
+
 class Config:
+    load_dotenv()
     SECRET_KEY = os.environ.get("SECRET")  # SECRET = "devkey123"
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = True
+
+# config_test = Config()
+# print(config_test.SECRET_KEY)
