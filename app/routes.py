4872ae7fe5fd0bee7cf1a8951709e412fd5128d3
IsaacRWI <isaacngrwi@gmail.com>
2025-11-10 11:14:06 +0000

    Added comments to routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,19 @@ from bleach import clean
 import logging
 from datetime import datetime
 
-safe_tags = {"b", "i", "u", "em", "strong", "a", "p", "ul", "ol", "li"}
+safe_tags = {"b", "i", "u", "em", "strong", "a", "p", "ul", "ol", "li"}  # safe tags for bio content
 
-logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')  # sets up logging
 
 def log_event(level, message, username=None):
+    """defines legging message format and log tags"""
     ip = request.remote_addr
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"{timestamp} Client IP:{ip}, User:{username or "N/A"} | {message}"
+    log_message = f"{timestamp} Client IP:{ip}, User:{username or "N/A"} | {message}"  # sets up logging message template
     if level == "info":
         logging.info(log_message)
     elif level == "warning":
-        logging.warning(log_message)
+        logging.warning(log_message)  # logs a warning message if warning tag passed in
 
 main = Blueprint('main', __name__)
 
@@ -25,25 +26,26 @@ def home():
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    sanitized_content = None
-    if form.validate_on_submit():
-        if form.bio.data:
+    """the form shown on the webpage"""
+    form = RegistrationForm()  # form = new instance of RegistrationForm
+    sanitized_content = None  # for sanitized bio content later on
+    if form.validate_on_submit():  # if form validates on submit
+        if form.bio.data:  # if bio input is not empty
             bio_content = form.bio.data
-            sanitized_content = clean(bio_content, tags=safe_tags, attributes={'a': ['href', 'title']}, strip=True)
+            sanitized_content = clean(bio_content, tags=safe_tags, attributes={'a': ['href', 'title']}, strip=True)  # cleaning bio input using bleach
 
-            if bio_content == sanitized_content:
+            if bio_content == sanitized_content:  # bio contained no disallowed tags
                 log_event("info", "Successful Registration", form.username.data)
                 flash(f"Registration Success, Username {form.username.data} registered", "info")
-            else:
+            else:  # bio contained disallowed tags
                 log_event("warning", "bio contained restricted tags", form.username.data)
-                sanitized_content = ""
+                sanitized_content = ""  # empties sanitized content
                 flash("Registration Failed, Bio content contained restricted tags", "warning")
-        else:
+        else:  # form validates and bio input is empty
             log_event("info", "Successful Registration, no bio", form.username.data)
             flash(f"Registration Success, Username {form.username.data} registered with no bio", "info")
 
-    elif request.method == "POST":
+    elif request.method == "POST":  # form validation failed
         for field, errors in form.errors.items():
             for i in errors:
                 log_event("warning", f"Validation Failed: {field} - {i}", form.username.data)
