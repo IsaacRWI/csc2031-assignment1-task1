288014e7b71db256fe61aba850d3e71c1993ac2c
IsaacRWI <isaacngrwi@gmail.com>
2025-11-10 11:18:21 +0000

    Added comments to config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
-    SECRET_KEY = os.environ.get("SECRET")  # SECRET = "devkey123"
+    SECRET_KEY = os.environ.get("SECRET")  # SECRET = "devkey123" in .env
     DEBUG = True
     TESTING = True
-    WTF_CSRF_ENABLED = True
+    WTF_CSRF_ENABLED = True  # csrf = true
 
 # config_test = Config()
 # print(config_test.SECRET_KEY)
